a85d592433134ea53a3e744a3181529f928ed5fb
moknisofien <moknisofien@gmail.com>
2019-11-11 11:21:37 +0100

    add dynamic js loader

--- src/app/layout/client/client.component.ts
+++ src/app/layout/client/client.component.ts
@@ -1,38 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import {ClientService} from '../../service/client.service';
+import {Component, OnInit} from '@angular/core';
+import {ClientService} from '../../services/client.service';
+import {DynamicScriptLoaderService} from '../../services/dynamic-script-loader.service';
 
 @Component({
-  selector: 'app-client',
-  templateUrl: './client.component.html',
-  styleUrls: ['./client.component.css']
+    selector: 'app-client',
+    templateUrl: './client.component.html',
+    styleUrls: ['./client.component.css']
 })
 export class ClientComponent implements OnInit {
-  clientList ;
-  p: number = 1;
-  s = 5;
-  filtredCategory: string = 'Category';
-  filtredStatus: string = 'Client status';
-  filtredGender: string = 'Sexe';
-  filredStartDate: Date;
-  filtredEndDate: Date;
-  constructor(private service: ClientService) { }
+    clientList;
+    p: number = 1;
+    s = 5;
+    filtredCategory: string = 'Category';
+    filtredStatus: string = 'Client status';
+    filtredGender: string = 'Sexe';
+    filredStartDate: Date;
+    filtredEndDate: Date;
 
-  ngOnInit() {
-   this.getAllClients();
-  }
-  getAllClients() {
-    this.service.getAllClients().subscribe(result => {
-      this.clientList = result ;
-    });
-  }
-  filters() {
-    console.log(this.filredStartDate);
-    this.service.getFiltredClients(this.filtredStatus).subscribe(res => {
-      console.log(res);
-      this.clientList = res;
-    });
-  }
+    constructor(private service: ClientService, private dynamicScriptLoader: DynamicScriptLoaderService) {
+    }
+
+    ngOnInit() {
+        this.getAllClients();
+        this.loadScripts();
+    }
+
+    getAllClients() {
+        this.service.getAllClients().subscribe(result => {
+            this.clientList = result;
+        });
+    }
+
+    filters() {
+        console.log(this.filredStartDate);
+        this.service.getFiltredClients(this.filtredStatus).subscribe(res => {
+            console.log(res);
+            this.clientList = res;
+        });
+    }
 
 
+    private loadScripts() {
+        // You can load multiple scripts by just providing the key as argument into load method of the service
+        this.dynamicScriptLoader.load('datetimepicker').then(data => {
+            // Script Loaded Successfully
+        }).catch(error => console.log(error));
+    }
 
 }
